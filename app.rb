f9a85e23dc7168098e5978ffaedbc74db971dbad
Przemyslaw Prencel <hartag96@gmail.com>
2019-06-24 16:12:54 +0200

    code clean up

--- app.rb
+++ app.rb
@@ -11,14 +11,6 @@ BIND_ADDR = '0.0.0.0'
 PORT = 3000
 TIMEOUT_SECONDS = 10
 
-socket = UDPSocket.new
-membership = IPAddr.new(MULTICAST_ADDR).hton + IPAddr.new(BIND_ADDR).hton
-# hton - returns a network byte ordered string form of the IP address.
-
-socket.setsockopt(:IPPROTO_IP, :IP_ADD_MEMBERSHIP, membership)
-socket.setsockopt(:SOL_SOCKET, :SO_REUSEPORT, 1)
-socket.bind(BIND_ADDR, PORT)
-
 def reader(socket)
   loop do
     message, _ = socket.recvfrom(255)
@@ -74,6 +66,13 @@ end
 # Main
 begin
   puts('Welcome to chat')
+  socket = UDPSocket.new
+  membership = IPAddr.new(MULTICAST_ADDR).hton + IPAddr.new(BIND_ADDR).hton
+  # hton - returns a network byte ordered string form of the IP address.
+
+  socket.setsockopt(:IPPROTO_IP, :IP_ADD_MEMBERSHIP, membership)
+  socket.setsockopt(:SOL_SOCKET, :SO_REUSEPORT, 1)
+  socket.bind(BIND_ADDR, PORT)
   query_nickname(socket)
 rescue SignalException
   socket.close
